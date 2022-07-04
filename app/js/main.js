69438a26be0982a932b6de85d11f8ac2cae39af2
Aleksandr Tloka <aleksandr.tloka@i.ua>
2022-07-04 12:39:25 +0300

    finished

--- app/js/main.js
+++ app/js/main.js
@@ -487,6 +487,43 @@ function scrollFunction() {
     document.querySelector('header').classList.remove('fixedMenu');
   }
 }
+
+var swiperTwo = new Swiper(".mySwiper3", {
+  watchSlidesProgress: true,
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-two-btn-next",
+    prevEl: ".swiper-two-btn-prev"
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      centeredSlides: true
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 640px
+    769: {
+      slidesPerView: 3,
+      spaceBetween: 15
+    },
+    993: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    }
+  }
+});
 })();
 
 /******/ })()
